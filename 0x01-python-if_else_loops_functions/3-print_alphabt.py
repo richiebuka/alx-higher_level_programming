899ecb69745ca3a4c30293c607bcabb3e0f863ae
richiebuka <rchiebuka@gmail.com>
2023-06-07 21:06:19 +0100

    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113:
+for alpha in rande(ord('a'), ord('z')+1):
+    if alpha in [101, 113]:
         continue
-    print("{:c}.format(i), end=" ")
+    print("{:c}".format(alpha), end="")
