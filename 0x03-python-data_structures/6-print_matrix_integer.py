5fbd634d851bf5a4b6928383ca1abf0f264f06e2
richiebuka <rchiebuka@gmail.com>
2023-06-12 11:09:06 +0100

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    if matrix != [[]]:
-        for row in matrix:
-            for i in row:
-                print("{:d}".format(i), end=" "
-                        if i != row[-1] else '\n')
-    else:
-        print()
+    if not matrix:
+        return None
+    for inside in matrix:
+        if len(inside) == 0:
+            print()
+        for i in range(len(inside)):
+            print("{:d}".format(inside[i]),
+                  end="\n" if i is len(inside) - 1 else " ")
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,6 @@ def max_integer(my_list=[]):
     if not len(my_list):
         return None
     else:
-        new_list = mylist.copy()
+        new_list = my_list.copy()
         new_list.sort()
         return new_list[-1]
