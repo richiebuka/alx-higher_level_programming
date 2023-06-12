a4ddff991de7f6fc1639154418b58224d07b9442
richiebuka <rchiebuka@gmail.com>
2023-06-12 10:20:19 +0100

    tuple

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix != [[]]:
+        for row in matrix:
+            for i in row:
+                print("{:d}".format(i), end=" "
+                        if i != row[-1] else '\n')
+    else:
+        print()
